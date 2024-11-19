52488983e36d25a3dcdbc7a5b912974ee9a95e60
Stefan Sjöberg <bird.nerd@outlook.com>
2024-11-19 14:29:47 +0100

    Add project files.

---
+++ AgeCondition/Program.cs
@@ -0,0 +1,8 @@
+﻿string current_age = "100";
+
+Console.Write("Ange din ålder: ");
+string input_age = current_age ?? "0";
+
+int age = int.Parse(input_age);
+
+Console.WriteLine("Din ålder är " + age);
